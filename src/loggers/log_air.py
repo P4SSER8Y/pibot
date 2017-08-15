3ad607e1bf129e77531eb75a0e5947c7e4d70450
psby233 <passerby233@outlook.com>
2017-08-15 08:51:14 +0800

    add air's logger

---
+++ src/loggers/log_air.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import rospy
+from pymongo import MongoClient
+from pibot import msg
+
+global collection 
+
+def callback(data):
+    global collection
+    collection.insert({'timestamp': data.timestamp,
+                       'temperature': data.temperature,
+                       'humidity': data.humidity})
+
+def init():
+    port = rospy.get_param('/pibot/mongodb_port')
+
+    client = MongoClient('localhost', port)
+    db = client.pibot
+    global collection
+    collection = db.air
+
+    rospy.init_node('log_air')
+    rospy.Subscriber('/pibot/air', msg.air, callback)
+
+if __name__ == '__main__':
+    init()
+    rospy.spin()
+
