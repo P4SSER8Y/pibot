eaac033c6ef6e0a6ed3af95f2f7b8d6756130b90
psby233 <passerby233@outlook.com>
2017-08-17 13:38:45 +0800

    add touchpad & gesture recognizer

---
+++ src/sensors/srv_apds9960.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+import rospy
+from std_msgs.msg import String
+
+from apds9960.const import *
+from apds9960 import APDS9960
+import RPi.GPIO as GPIO
+import smbus
+from time import sleep
+
+dirs = {
+    APDS9960_DIR_NONE: "none",
+    APDS9960_DIR_LEFT: "left",
+    APDS9960_DIR_RIGHT: "right",
+    APDS9960_DIR_UP: "up",
+    APDS9960_DIR_DOWN: "down",
+    APDS9960_DIR_NEAR: "near",
+    APDS9960_DIR_FAR: "far",
+}
+
+port = 1
+bus = smbus.SMBus(port)
+
+apds = None
+pub = None
+
+def handler(channel):
+    if apds.isGestureAvailable():
+        motion = apds.readGesture()
+        pub.publish(dirs.get(motion, "none"))
+
+
+if __name__ == "__main__":
+    pin = rospy.get_param('/pibot/pins/gesture_int')
+    apds = APDS9960(bus)
+
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(pin, GPIO.IN)
+    GPIO.add_event_detect(pin, GPIO.FALLING, callback = handler)
+
+    apds.setProximityIntLowThreshold(50)
+
+    rospy.init_node('srv_apds9960')
+    pub = rospy.Publisher('/pibot/key', String, queue_size=1)
+    apds.enableGestureSensor()
+
+    rospy.spin()
+
+    GPIO.cleanup()
+
---
+++ src/sensors/srv_touchpad.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+import rospy
+from pibot import msg
+from RPi import GPIO
+import time
+
+ts = 0
+pub = None
+
+def handler(pin):
+    global ts
+    global pub
+    tmp = time.time()
+    res = msg.touchpad()
+    if GPIO.input(pin) == 1:
+        res.value = 0
+    else:
+        res.value = 1
+    res.width = tmp - ts
+    pub.publish(res)
+    ts = tmp
+
+def init():
+    pin = rospy.get_param('/pibot/pins/Touchpad')
+
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(pin, GPIO.IN)
+    GPIO.add_event_detect(pin, GPIO.BOTH, callback = handler)
+
+    rospy.init_node('srv_touchpad')
+    global pub
+    pub = rospy.Publisher('/pibot/touchpad', msg.touchpad, queue_size=10)
+
+
+if __name__ == '__main__':
+    ts = time.time()
+    init()
+    rospy.spin()
+        
