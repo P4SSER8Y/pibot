8ea821aff805354ecf6dc914ad221d84185bed8b
psby233 <passerby233@outlook.com>
2017-08-19 21:25:31 +0800

    add lcd1602

---
+++ src/lcd1602/srv_lcd1602.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+from Adafruit_CharLCD import Adafruit_CharLCD
+import rospy
+from std_msgs.msg import String
+
+lcd = None
+buff = '  PiBot Loaded  \n  Hello World!  '
+
+def handler(data):
+    global buff
+    global lcd
+    lcd.setCursor(0, 0)
+    lcd.write(data.data)
+    buff = data.data
+
+
+def init():
+    global lcd
+    pins = rospy.get_param('/pibot/pins/LCD1602')
+    lcd = Adafruit_CharLCD(pin_rs=pins['RS'], pin_e=pins['EN'], pins_db=pins['DATA'], GPIO=None)
+    lcd.begin(16, 2)
+    lcd.clear()
+    lcd.write('  PiBot Loaded  \n  Hello World!  ')
+    lcd.setCursor(0, 0)
+
+    rospy.init_node('lcd1602')
+    sub = rospy.Subscriber('/pibot/lcd1602', String, handler)
+    
+
+if __name__ == '__main__':
+    init()
+    rospy.spin()
+
+
