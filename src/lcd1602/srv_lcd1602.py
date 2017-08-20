fc4f945b295d3dad1910b317928d8efe1be35252
psby233 <passerby233@outlook.com>
2017-08-20 14:24:13 +0800

    reset namespaces; add shutdown message

--- src/lcd1602/srv_lcd1602.py
+++ src/lcd1602/srv_lcd1602.py
@@ -4,6 +4,7 @@ from Adafruit_CharLCD import Adafruit_CharLCD
 import rospy
 from std_msgs.msg import String
 
+sub = None
 lcd = None
 buff = '  PiBot Loaded  \n  Hello World!  '
 
@@ -17,6 +18,7 @@ def handler(data):
 
 def init():
     global lcd
+    global sub
     pins = rospy.get_param('/pibot/pins/LCD1602')
     lcd = Adafruit_CharLCD(pin_rs=pins['RS'], pin_e=pins['EN'], pins_db=pins['DATA'], GPIO=None)
     lcd.begin(16, 2)
@@ -31,5 +33,8 @@ def init():
 if __name__ == '__main__':
     init()
     rospy.spin()
+    sub.unregister()
+    lcd.clear()
+    lcd.write('PiBot Shutdowned\nAuf Wiedersehen!')
 
 
